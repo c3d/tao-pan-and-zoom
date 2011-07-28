c1bf53a4fdf3bb56866262e50c6ae5c408d4c9bf
Christophe de Dinechin <christophe@taodyne.com>
2011-07-28 16:18:41 +0200

    Pan and zoom example and documentation, closes #1188

---
+++ doc/pan_and_zoom.doxy.h
@@ -0,0 +1,82 @@
+/**
+ * @defgroup PanAndZoom Pan and Zoom
+ * @ingroup Modules
+ *
+ * Pan-and-Zoom navigation style for Tao Presentations
+ *
+ * PanAndZoom (PAZ) is an interesting presentation navigation model
+ * 
+ * The module provides a ready-to-use navigation model
+ *
+ * Here is an example (<a href="examples/pan_and_zoom.ddd">pan_and_zoom.ddd</a>):
+ * @include pan_and_zoom.ddd
+ *
+ * Here is a screen capture of this document:
+ * @image html "pan_and_zoom.png" "Output from pan_and_zoom.ddd"
+ * @{
+ */
+
+
+/**
+ * Create a page containing Pan-and-Zoom contents
+ *
+ * The contents of the page is defined by the @a Body variable.
+ * There can be only one @ref paz_page per @ref page.
+ */
+
+paz_page(tree Body);
+
+
+/**
+ * Add an item in a @ref paz_page. 
+ *
+ * The item specified by @a Body is added at coordinates @a X and @a Y.
+ * It is rotated by @a Angle degrees, and scaled down by @a Scale.
+ * Items draw in @a Body can be selected by a mouse click.
+ */
+paz (real X, real Y, real Angle, real Scale, tree Body);
+
+
+/**
+ * Add a text box in a pan-and-zoom context
+ *
+ * The text box has dimension @a Width x @a Height units and is centered
+ * when selected. The contents is defined by @a Body.
+ */
+paz_box(real Width, real Height, tree Body);
+
+
+/**
+ * Add a text box with a rounded rectangle around it
+ *
+ * This is similar to @ref paz_box, but with an
+ * additional @ref rounded_rectangle drawn around it.
+ */
+paz_rbox(real Width, real Height, tree Body);
+
+
+/**
+ * Execute code in @a Body only if the current @ref paz is selected.
+ */
+paz_cond(tree Body);
+
+
+/**
+ * Test if the current @ref paz is between @a Start and @a End from this one
+ * 
+ * The @a Start and @a End parameters indicate an offset relative to the
+ * current @ref paz box.
+ */
+paz_between(real Start, real End);
+
+
+/// Variables
+
+/**
+ * Index of the current @ref paz item being executed
+ */
+integer pax_index;
+
+/**
+ * @}
+ */
---
+++ pan_and_zoom.pro
@@ -0,0 +1,41 @@
+# ******************************************************************************
+#  pan_and_zoom.pro                                                Tao project
+# ******************************************************************************
+# File Description:
+#
+#    Qt build file for the PanAndZoom module
+#
+#    PanAndZoom offers an interesting way to navigate in a slide
+#
+#
+#
+# ******************************************************************************
+# This software is property of Taodyne SAS - Confidential
+# Ce logiciel est la propriété de Taodyne SAS - Confidentiel
+# (C) 2011 Christophe de Dinechin <christophe@taodyne.com>
+# (C) 2011 Taodyne SAS
+# ******************************************************************************
+
+MODINSTDIR = pan_and_zoom
+
+include(../modules.pri)
+
+OTHER_FILES = pan_and_zoom.xl \
+    doc/pan_and_zoom.png \
+    doc/pan_and_zoom.ddd \
+    icon.png
+
+PICTURES = \
+    images/Planet-Map.jpg \
+    images/Planet-Clouds.png \
+    images/Movie-Stars-Background.jpg \
+    $$NULL
+
+pics.path  = $$MODINSTPATH/images
+pics.files = $$PICTURES
+
+INSTALLS    += thismod_icon pics
+INSTALLS    -= thismod_bin
+
+HEADERS += \
+    doc/pan_and_zoom.doxy.h
